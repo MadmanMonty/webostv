3522fe69bc6568ede86a3451dee7f0817a59f7aa
MadManMonty <MadmanMonty@users.noreply.github.com>
2019-11-17 20:22:20 +0000

    Cleanup of unneeded remote functionality

--- remote.py
+++ remote.py
@@ -13,7 +13,6 @@ from homeassistant.components import remote
 from homeassistant import util
 
 from homeassistant.const import (
-    CONF_CUSTOMIZE,
     CONF_FILENAME,
     CONF_HOST,
     CONF_NAME,
@@ -39,28 +38,18 @@ from homeassistant.helpers.script import Script
 _CONFIGURING: Dict[str, str] = {}
 _LOGGER = logging.getLogger(__name__)
 
-CONF_SOURCES = "sources"
-CONF_ON_ACTION = "turn_on_action"
-
 DEFAULT_NAME = "LG webOS Smart TV"
-LIVETV_APP_ID = "com.webos.app.livetv"
 
-WEBOSTV_CONFIG_FILE = "webostv1.conf"
+WEBOSTV_CONFIG_FILE = "webostv.conf"
 
 MIN_TIME_BETWEEN_SCANS = timedelta(seconds=10)
 MIN_TIME_BETWEEN_FORCED_SCANS = timedelta(seconds=1)
 
-CUSTOMIZE_SCHEMA = vol.Schema(
-    {vol.Optional(CONF_SOURCES): vol.All(cv.ensure_list, [cv.string])}
-)
-
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
-        vol.Optional(CONF_CUSTOMIZE, default={}): CUSTOMIZE_SCHEMA,
         vol.Optional(CONF_FILENAME, default=WEBOSTV_CONFIG_FILE): cv.string,
         vol.Optional(CONF_HOST): cv.string,
         vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-        vol.Optional(CONF_ON_ACTION): cv.SCRIPT_SCHEMA,
         vol.Optional(CONF_TIMEOUT, default=8): cv.positive_int,
     }
 )
@@ -82,17 +71,15 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
         return
 
     name = config.get(CONF_NAME)
-    customize = config.get(CONF_CUSTOMIZE)
     timeout = config.get(CONF_TIMEOUT)
-    turn_on_action = config.get(CONF_ON_ACTION)
 
     config = hass.config.path(config.get(CONF_FILENAME))
 
-    setup_tv(host, name, customize, config, timeout, hass, add_entities, turn_on_action)
+    setup_tv(host, name, config, timeout, hass, add_entities)
 
 
 def setup_tv(
-    host, name, customize, config, timeout, hass, add_entities, turn_on_action
+    host, name, config, timeout, hass, add_entities
 ):
     """Set up a LG WebOS TV based on host parameter."""
     from pylgtv import WebOsClient
@@ -118,12 +105,10 @@ def setup_tv(
             request_configuration(
                 host,
                 name,
-                customize,
                 config,
                 timeout,
                 hass,
                 add_entities,
-                turn_on_action,
             )
             return
 
@@ -134,13 +119,13 @@ def setup_tv(
         configurator.request_done(request_id)
 
     add_entities(
-        [LgWebOSRemote(host, name, customize, config, timeout, hass, turn_on_action)],
+        [LgWebOSRemote(host, name, config, timeout, hass)],
         True,
     )
 
 
 def request_configuration(
-    host, name, customize, config, timeout, hass, add_entities, turn_on_action
+    host, name, config, timeout, hass, add_entities
 ):
     """Request configuration steps from the user."""
     configurator = hass.components.configurator
@@ -155,7 +140,7 @@ def request_configuration(
     def lgtv_configuration_callback(data):
         """Handle actions when configuration callback is called."""
         setup_tv(
-            host, name, customize, config, timeout, hass, add_entities, turn_on_action
+            host, name, config, timeout, hass, add_entities
         )
 
     _CONFIGURING[host] = configurator.request_config(
@@ -169,13 +154,10 @@ def request_configuration(
 class LgWebOSRemote(remote.RemoteDevice):
     """Representation of a LG WebOS TV."""
     from custom_components.webostv.pylgtv.webos_client import WebOsClient
-    def __init__(self, host, name, customize, config, timeout, hass, on_action):
+    def __init__(self, host, name, config, timeout, hass):
         """Initialize the webos device."""
 
         self.remote = WebOsClient(host, config, timeout)
-        # self._on_script = Script(hass, on_action) if on_action else None
-        # self._customize = customize
-
         self._name = name
 
     @util.Throttle(MIN_TIME_BETWEEN_SCANS, MIN_TIME_BETWEEN_FORCED_SCANS)
@@ -200,13 +182,9 @@ class LgWebOSRemote(remote.RemoteDevice):
 
     def send_command(self, command, **kwargs):
         """Send a command to one device."""
-        _LOGGER.warning("send_command:" + str(command))
+        _LOGGER.debug("send_command:" + str(command))
         #self.remote.up_button()
         
         for single_command in command:
-            #if not hasattr(self.remote, single_command):
-            #   continue
-
-            #getattr(self.remote, single_command)()
-            _LOGGER.warning("send_command:" + str(single_command))
+            _LOGGER.debug("send_command:" + str(single_command))
             self.remote.send_button(single_command)
